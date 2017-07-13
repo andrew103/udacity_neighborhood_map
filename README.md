f12ea731ea36192ca1293a46542dd75419041b4e
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-13 14:27:31 -0700

    Add codepen hamburger menu code

---
+++ README.md
@@ -0,0 +1,2 @@
+Hamburger code attributed to Glenn Smith from codepen.io
+LINK: https://codepen.io/g13nn/pen/eHGEF
---
+++ js/app.js
@@ -0,0 +1,19 @@
+// BEGIN CODEPEN.IO CODE
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+// END CODEPEN.IO CODE
