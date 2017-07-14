35ccb3ee961b01cec6225c1faceb12c58c5f2e0d
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-13 19:23:32 -0700

    Add test locations

--- js/app.js
+++ js/app.js
@@ -24,7 +24,17 @@ var markers = [];
 
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 34.0522, lng: -118.2437},
+        // My location: {lat: 34.0522, lng: -118.2437}
+      center: {lat: 40.7413549, lng: -73.9980244},
       zoom: 11,
   });
+
+  var locations = [
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+  ];
 }
